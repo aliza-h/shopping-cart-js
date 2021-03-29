6e8c592d82e04e282f0ada2955480ea574d3b179
Aliza Howe <alizahowe26@gmail.com>
2021-03-29 14:32:18 -0400

    Basic setup

---
+++ README.md
@@ -0,0 +1,33 @@
+**GOAL:** online shopping cart
+
+- 10 to 15 items available
+- Button for each item (name it "Add to cart")
+- Clicking on "add to cart" saves the item to a cart (dictionary/array)
+- "Next" button at bottom of homepage
+
+**PAGES:**
+- homepage (list of items)
+- cart page ("proceed to checkout" at bottom)
+- checkout page (print the cart items)
+
+
+**REFERENCES:**
+
+https://github.com/fiarh/cset/blob/master/110/css-selectors/selectors.css
+
+https://github.com/fiarh/cset/blob/master/110/css-selectors/selectors.html
+
+
+Haimatsu Technologies
+- Omnitool
+- ZA-X2 Experimental Module
+- Ductile Suit
+- Power Suit
+- Energy Pal S.3 Battery Pack
+- In-Ground Station Terminal
+- Standard Field Tablet (7x5")
+
+Carthage Industries:
+- Structure Gel (1L)
+- Structure Gel (5L)
+- CCRV-7 Blackbox
---
+++ cart-script.js
@@ -0,0 +1,21 @@
+// Defining page sections/screens
+let catalogueDisplay = document.querySelector("#catalogue");
+let cartDisplay = document.querySelector("#user-cart");
+let paidDisplay = document.querySelector("#complete");
+
+// Defining cart buttons
+let hOmnitool = document.querySelector("#h-omnitool");
+let hARK = document.querySelector("#h-ark");
+let hDiveSuit = document.querySelector("#h-dive-suit");
+let hPowerSuit = document.querySelector("#h-power-suit");
+let hBatteryPack = document.querySelector("#h-battery-pack");
+let hTerminal = document.querySelector("#h-terminal");
+let hTablet = document.querySelector("#h-tablet");
+let cGel1 = document.querySelector("#c-gel-1");
+let cGel5 = document.querySelector("#c-gel-5");
+let cBlackbox = document.querySelector("#c-blackbox");
+
+// Defining page buttons
+let viewCart = document.querySelector("#view-cart");
+let toCheckout = document.querySelector("#checkout");
+let homeButton = document.querySelector("#home-button");
